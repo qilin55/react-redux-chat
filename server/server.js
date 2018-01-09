aabb0e9c44f901f62cd0ada226264199067bf2b8
qilin <sunqilin@jeeshare.com>
2018-01-09 22:17:38 +0800

    使用mongoose连接MongoDB

--- server/server.js
+++ server/server.js
@@ -1,4 +1,40 @@
 const express = require('express')
+const mongoose = require('mongoose')
+//链接mongo 并且使用coby这个集合
+const DB_URL = 'mongodb://localhost:27017/coby'
+mongoose.connect(DB_URL)
+mongoose.connection.on('connected',function () {
+    console.info('mongo connect success')
+})
+
+const User = mongoose.model('user',new mongoose.Schema({
+    user:{type:String,require:true},
+    age:{type:Number,require:true}
+}))
+
+//新增一条数据到数据库
+
+// User.create({
+//     age:12,
+//     user:'coby'
+// },function (err, doc) {
+//     if(!err){
+//         console.info(doc)
+//     }else{
+//         console.info(errs)
+//     }
+// })
+
+//删除数据
+// User.remove({age:18},function (err,doc) {
+//     console.info(doc)
+// })
+
+//更新数据
+// User.update({'user':'nami'},{'$set':{age:18}},function (err,doc) {
+//     console.info(doc)
+// })
+
 // 新建APP
 const app = express()
 
@@ -6,8 +42,13 @@ app.get('/',function(req,res){
     res.send('<h1>Hello world</h1>')
 })
 
+//查找数据 find() 是查找出符合条件的对象集合，是一个[],findOne() 是查找出来一条数据，是一个{}
+
 app.get('/data',function(req,res){
-    res.json({name:'coby',work:'frontend',age:18})
+    User.find({},function (err,doc) {
+       res.json(doc)
+    })
+    // res.json({name:'coby',work:'frontend',age:18})
 })
 
 app.listen(9000,function(){
